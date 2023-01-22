491ba25b99a4093be38e033a1f444edb680cb320
Aashutosh Rathi <aashutoshrathi@gmail.com>
2023-01-22 15:06:35 +0530

    :zap: feat: update ip source and refactor options.js

--- src/options/index.js
+++ src/options/index.js
@@ -1,15 +1,12 @@
 const getCurrentIp = async () => {
-  const apiData = await fetch("https://www.cloudflare.com/cdn-cgi/trace");
-  const textData = await apiData.text();
-  const data = textData
-    .trim()
-    .split("\n")
-    .reduce(function (obj, pair) {
-      pair = pair.split("=");
-      return (obj[pair[0]] = pair[1]), obj;
-    }, {});
-
-  return data.ip;
+  try {
+    const apiData = await fetch("https://hutils.loxal.net/whois");
+    const data = await apiData.json();
+    return data.ip;
+  } catch (e) {
+    console.error(e.message);
+    return "";
+  }
 };
 
 const updateIpField = async () => {
@@ -17,50 +14,54 @@ const updateIpField = async () => {
   document.querySelector("#ip").value = ip;
 };
 
-document
-  .querySelector('input[name="name"]')
-  .addEventListener("keydown", function () {
-    const name = document.querySelector('input[name="name"]').value;
-    chrome.storage.sync.set({ name: name });
-  });
-
-document.querySelector("#upsert").addEventListener("click", async () => {
-  try {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    });
-
-    if (tab.url.indexOf("mongodb.com") === -1) {
-      return;
-    }
-
-    const name = document.querySelector('input[name="name"]').value;
-    const ip = document.querySelector("input[name='ip']").value;
-    await chrome.tabs.sendMessage(tab.id, {
-      action: "upsert",
-      values: {
-        name,
-        ip,
-        isCurrentIp: ip === await getCurrentIp(),
-      },
-    });
-    window.close();
-  } catch (e) {
-    document.querySelector("#error").innerText = e.message;
-  }
-});
-
-document.querySelector("#refresh-ip").addEventListener("click", async () => {
-  updateIpField();
-});
 
 document.addEventListener("DOMContentLoaded", function () {
+  // get current value from storage & fill it in input field
   chrome.storage.sync.get("name", function (data) {
     if (data.name) {
       document.querySelector('input[name="name"]').value = data.name;
     }
   });
 
+  // Fill in the current IP
   updateIpField();
+
+
+  document
+    .querySelector('input[name="name"]')
+    .addEventListener("keyup", function () {
+      const name = document.querySelector('input[name="name"]').value;
+      chrome.storage.sync.set({ name: name });
+    });
+
+  document.querySelector("#upsert").addEventListener("click", async () => {
+    try {
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+      });
+
+      if (tab.url.indexOf("mongodb.com") === -1) {
+        return;
+      }
+
+      const name = document.querySelector('input[name="name"]').value;
+      const ip = document.querySelector("input[name='ip']").value;
+      await chrome.tabs.sendMessage(tab.id, {
+        action: "upsert",
+        values: {
+          name,
+          ip,
+          isCurrentIp: ip === await getCurrentIp(),
+        },
+      });
+      window.close();
+    } catch (e) {
+      document.querySelector("#error").innerText = e.message;
+    }
+  });
+
+  document.querySelector("#refresh-ip").addEventListener("click", async () => {
+    updateIpField();
+  });
 });
